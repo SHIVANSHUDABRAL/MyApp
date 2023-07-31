67288a176bac44938f1d1e6a09c42d4715a80306
SHIVANSHUDABRAL <Shivansh05627gmail.com>
2023-07-31 11:48:53 +0530

    Updates

---
+++ precache-manifest.6ea43ca48f25bbf4086db7c81c3fbc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03831061b832649bdbfb",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/static/css/main.0817b51c.chunk.css"
+  },
+  {
+    "revision": "03831061b832649bdbfb",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/static/js/main.3186958e.chunk.js"
+  },
+  {
+    "revision": "523c641f378f4750a1c8",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/static/js/runtime~main.5480ed07.js"
+  },
+  {
+    "revision": "9a9707ffc9a823fc1f21",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/static/js/2.12692025.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "4300e321ef4917a1f8d06b0b29134532",
+    "url": "/SHIVANSHUDABRAL/MyApp.git/index.html"
+  }
+];
